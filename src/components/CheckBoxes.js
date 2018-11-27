01adcfd729b61976fbfee90e992bcbc47617e141
DESKTOP-I39GA3T\Рурик <igorurr@yandex.ru>
2018-11-27 09:28:01 +0300

    добавлены фильтры с логикой обновления значений при изменении элементов

---
+++ src/components/CheckBoxes.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react'
+
+import { tryRemoveFromArray, tryPushToArray } from '../helpers'
+
+class CheckBoxes extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: props.data,
+      actives: 'default' in props ? props.default : []
+    };
+
+    this.updateInputValue = this.updateInputValue.bind(this);
+  }
+
+  getNewStateActive( elem, state )
+  {
+    let newState = this.state.actives.slice();
+
+    if( state==true )
+      tryPushToArray(newState,elem);
+    else
+      tryRemoveFromArray(newState,elem);
+
+    return newState;
+  }
+
+  updateInputValue(evt) {
+    const newActives = this.getNewStateActive(evt.target.value, evt.target.checked);
+
+    this.setState({
+      actives: newActives
+    });
+
+    if( 'onChange' in this.props )
+      this.props.onChange(newActives);
+  }
+
+  render() {
+    return (
+      <content className={"input-checkbox"}>
+        {this.state.data.map((el,i,arr) => (
+          <label className={"input-checkbox-elem"}>
+            <input
+              type={"checkbox"}
+              checked={this.state.actives.indexOf(el.value)!==-1}
+              onChange={this.updateInputValue}
+              value={el.value}
+            />
+            {el.text}
+          </label>
+        ))}
+      </content>
+    );
+  }
+};
+
+export default CheckBoxes;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,6 @@
 import LeftBar from './LeftBar';
+import Navigate from './Navigate';
 import Page1 from './Page1';
 import Page2 from './Page2';
 
-export { LeftBar, Page1, Page2 }
+export { LeftBar, Navigate, Page1, Page2 }
