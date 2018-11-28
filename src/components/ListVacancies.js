14f18a1916c27951ce0c985500f588de4a550dbb
realtosy <tosyreal@gmail.com>
2018-11-28 23:15:10 +0300

    Лист с вакансиями

--- src/components/ListVacancies.js
+++ src/components/ListVacancies.js
@@ -1,33 +1,53 @@
 import React, {Component} from 'react';
 
-class ListVacancies extends Component {
+class RowComponent extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      list: [
-        {
-          name: 'ds5f',
-          position: [55.75, 37.57],
-          description: 'dfdfgfdfgdfgfg'
-        },
-        {
-          name: 'ds4f',
-          position: [55.75, 38.57],
-          description: 'dfdfgfdfgdfgfg'
-        },
-        {
-          name: 'dsf6',
-          position: [56.75, 37.57],
-          description: 'dfdfgfdfgdfgfg'
-        }
-      ]
-    };
   }
 
   render() {
     return (
-      <p />
+      <li>{this.props.item.name} from {this.props.item.position} to {this.props.item.description}</li>
+      <button onClick={this.props.onAdd}>Add</button>
+      <button onClick={this.props.onRemove}>Remove</button>
+    )
+  }
+}
+
+class ListVacancies extends React.Component {
+  constructor(props) {
+    super(props);
+    const {isFetching, 
+      page, 
+      items, 
+      loadNextPage, 
+      addIfNotExist, 
+      removeIfExist} = this.props;
+
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  handleScroll() {
+    const root = document.getElementById('root');
+    const bottomScrollMark = 200;
+    const currentBottomScroll = root.scrollHeight - root.scrollTop - root.clientHeight;
+
+    if (isFetching || currentBottomScroll > bottomScrollMark) {
+      return;
+    }
+    loadNextPage(page + 1);
+  }
+
+  render() {
+    return (
+      <div onScroll={this.handleScroll}>
+        <ul>
+          {this.props.items.map(
+            item => (
+              <RowComponent key={item.id} item={item} onAdd={addIfNotExist} onRemove={this.removeIfExist}/>
+            ))}
+        </ul>
+      </div>
     );
   }
 };
