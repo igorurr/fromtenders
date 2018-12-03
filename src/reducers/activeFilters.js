6de3a2af76bed07c387178386fc9ba7f08f3847e
immitsu <tsulayame@gmail.com>
2018-12-03 05:08:08 +0300

    Фикс предыдущего. Откат фильтров

--- src/reducers/activeFilters.js
+++ src/reducers/activeFilters.js
@@ -1,6 +1,14 @@
 import { Map } from 'immutable';
 import { UPDATE_FILTERS } from '../constants/filters';
 
+// const initialState = Map({
+//   exp: '',
+//   empl: '',
+//   schedule: '',
+//   salary: '',
+//   sortType: ''
+// });
+
 const initialState = Map({
   exp: '',
   empl: '',
@@ -12,7 +20,11 @@ const initialState = Map({
 const activeFilters = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_FILTERS:
-      return state.set(action.filter, action.value)
+      // return state.set(action.filter, action.value)
+      return {
+        ...state,
+        [action.filter]: action.value
+      }
     default:
       return state;
   }
