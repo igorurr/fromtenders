7eecce4936f95aa78361f2ad67d3f2a5bd95136f
immitsu <tsulayame@gmail.com>
2018-12-03 06:29:09 +0300

    Фикс предыдущего_2

--- src/reducers/activeFilters.js
+++ src/reducers/activeFilters.js
@@ -1,26 +1,16 @@
-import { Map } from 'immutable';
 import { UPDATE_FILTERS } from '../constants/filters';
 
-// const initialState = Map({
-//   exp: '',
-//   empl: '',
-//   schedule: '',
-//   salary: '',
-//   sortType: ''
-// });
-
-const initialState = Map({
+const initialState = {
   exp: '',
   empl: '',
   schedule: '',
   salary: '',
   sortType: ''
-});
+};
 
 const activeFilters = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_FILTERS:
-      // return state.set(action.filter, action.value)
       return {
         ...state,
         [action.filter]: action.value
